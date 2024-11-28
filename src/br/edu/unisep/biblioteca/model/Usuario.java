fecf3f829cbe621a1827c0bfd36fdaafaef9f462
JohnVBS <joaovitor.save@hotmail.com>
2024-11-28 20:45:12 -0300

    jooj

--- src/br/edu/unisep/biblioteca/model/Usuario.java
+++ src/br/edu/unisep/biblioteca/model/Usuario.java
@@ -1,4 +1,38 @@
 package br.edu.unisep.biblioteca.model;
 
 public class Usuario {
+    private String nome;
+    private int idade;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                '}';
+    }
 }
